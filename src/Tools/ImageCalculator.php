f17b2ba4ae2a2e40d973d588acaf0332c6c8fb7b
rkr <ron.kirschler@gmail.com>
2022-07-22 17:08:16 +0200

    `ImageCalculator::*`: Changed the behaviour when both target-width and target-height are `null`. Before, the result was array{null, null}, now it's array{srcW, srcH}

--- src/Tools/ImageCalculator.php
+++ src/Tools/ImageCalculator.php
@@ -8,7 +8,7 @@ class ImageCalculator {
 	 * @param int $origHeight
 	 * @param int|null $targWidth
 	 * @param int|null $targHeight
-	 * @return array{int, int}|array{null, null}
+	 * @return array{int, int}
 	 */
 	public static function getProportionalSize(int $origWidth, int $origHeight, ?int $targWidth, ?int $targHeight): array {
 		if($targWidth !== null && $targHeight === null) {
@@ -17,7 +17,7 @@ class ImageCalculator {
 			$targWidth = (int) round($targHeight / $origHeight * $origWidth);
 		} elseif($targWidth === null && $targHeight === null) {
 			// No new width and height given. Retain measures as is as if resize was commanded with original width and height.
-			return [null, null];
+			return [$origWidth, $origHeight];
 		}
 
 		if($targWidth > $targHeight) {
@@ -45,40 +45,49 @@ class ImageCalculator {
 	}
 	
 	/**
-	 * @param int $srcW
-	 * @param int $srcH
-	 * @param int|null $dstW
-	 * @param int|null $dstH
-	 * @return array{int, int}|array{null, null}
+	 * @param int $origWidth
+	 * @param int $origHeight
+	 * @param int|null $targWidth
+	 * @param int|null $targHeight
+	 * @return array{int, int}
 	 */
-	public static function getProprtionalCoverSize(int $srcW, int $srcH, ?int $dstW, ?int $dstH): array {
-		[$finalW, $finalH] = self::getProprtionalCoverSizeF($srcW, $srcH, $dstW, $dstH);
+	public static function getProprtionalCoverSize(int $origWidth, int $origHeight, ?int $targWidth, ?int $targHeight): array {
+		[$finalW, $finalH] = self::getProprtionalCoverSizeF($origWidth, $origHeight, $targWidth, $targHeight);
 		return [(int) $finalW, (int) $finalH];
 	}
 	
 	/**
-	 * @param int $srcW
-	 * @param int $srcH
-	 * @param int|null $dstW
-	 * @param int|null $dstH
-	 * @return array{float, float}|array{null, null}
+	 * @param int $origWidth
+	 * @param int $origHeight
+	 * @param int|null $targWidth
+	 * @param int|null $targHeight
+	 * @return array{float, float}
 	 */
-	public static function getProprtionalCoverSizeF(int $srcW, int $srcH, ?int $dstW, ?int $dstH): array {
-		if($dstH >= $dstW) {
-			$finalH = $dstH;
-			$finalW = $finalH * $srcW / $srcH;
+	public static function getProprtionalCoverSizeF(int $origWidth, int $origHeight, ?int $targWidth, ?int $targHeight): array {
+		if($targWidth !== null && $targHeight === null) {
+			$targHeight = (int) round($targWidth / $origWidth * $origHeight);
+		} elseif($targWidth === null && $targHeight !== null) {
+			$targWidth = (int) round($targHeight / $origHeight * $origWidth);
+		} elseif($targWidth === null && $targHeight === null) {
+			// No new width and height given. Retain measures as is as if resize was commanded with original width and height.
+			return [$origWidth, $origHeight];
+		}
+		
+		if($targHeight >= $targWidth) {
+			$finalH = $targHeight;
+			$finalW = $finalH * $origWidth / $origHeight;
 			
-			if(!($finalW >= $dstW)) {
-				$finalW = $dstW;
-				$finalH = $finalW * $srcH / $srcW;
+			if(!($finalW >= $targWidth)) {
+				$finalW = $targWidth;
+				$finalH = $finalW * $origHeight / $origWidth;
 			}
 		} else {
-			$finalW = $dstW;
-			$finalH = $finalW * $srcH / $srcW;
+			$finalW = $targWidth;
+			$finalH = $finalW * $origHeight / $origWidth;
 			
-			if(!($finalH >= $dstH)) {
-				$finalH = $dstH;
-				$finalW = $finalH * $srcW / $srcH;
+			if(!($finalH >= $targHeight)) {
+				$finalH = $targHeight;
+				$finalW = $finalH * $origWidth / $origHeight;
 			}
 		}
 		
